06d708bbd426eb22ecab7e0cec850a2b0aa6fe73
jsparks404 <sparksj404@gmail.com>
2023-02-04 17:48:56 -0500

    getting inputs from users

---
+++ input.py
@@ -0,0 +1,20 @@
+                # Getting input from user
+
+
+# name = input("Enter your name: ") # Prompts user to type in their name
+# print ("Hello " + name + "!")
+
+# num1 = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+# result = float(num1) + float(num2)         # int(num) returns integer   float(num) returns decimal 
+# print(result)
+
+
+
+color = input('Enter a color: ')
+plural_noun = input('Enter a plural noun: ')
+celebrity = input('Enter a celebrity: ')
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
