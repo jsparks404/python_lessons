d2973b2c48980a714602c3a43f9790c9b2b15683
jsparks404 <sparksj404@gmail.com>
2023-02-10 14:59:58 -0500

    random

---
+++ beginner.py
@@ -0,0 +1,56 @@
+            #strings
+
+#  /n creates new line where the /n is
+# concatonation     "hello " + "Josh" =  "hello Josh"
+
+
+            #input 
+
+# input("What is your name? ") -> allows for user input 
+
+# print("Hello " + input("What is your name? ")) -> Hello "user input"
+
+# print(len(input("whats your name")))
+
+# city = input("What city did you grow up in")
+# pet = input("whats the name of your first pet")
+
+# print(city + ' ' + pet)
+
+
+# string
+# number
+# float
+# boolean
+
+# type() return data type of input
+# type("hello") -> string
+# type(123) -> number
+
+# round(8 / 3) -> 3
+
+
+            # F string
+# f"your score is {number}"
+# print(f"your score is {4}")
+
+
+# age = int(input("how old are you"))
+
+# days = age * 365
+# weeks = age * 52
+# months = age * 12
+
+# print(f"you are {days} days old, {weeks} weeks old, and {months} months old!")
+
+
+            # Randomisation      must import random module
+
+# import random  # can import other files and use variables and or functions in those files
+# random_integer = random.randint(1, 10) # gives you random integer between 1 and 10 including those numbers
+# random_float = random.random()
+# print(random_float)
+
+
+
+         
