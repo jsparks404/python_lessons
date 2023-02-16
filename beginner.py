4b16656a8adc1012cc5e5e7051f649b3f0305222
jsparks404 <sparksj404@gmail.com>
2023-02-16 16:36:04 -0500

    while maze

--- beginner.py
+++ beginner.py
@@ -178,3 +178,23 @@ while number_of_hurdles > 0:
 # goal is placed after a random hurdle
 while not at_goal:
     jump()
+
+
+    #Maze
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
