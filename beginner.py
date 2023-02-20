b2e9f8426fb59e381149647a14db962cf09bad6b
jsparks404 <sparksj404@gmail.com>
2023-02-20 17:40:02 -0500

    shift = shift % 26

--- beginner.py
+++ beginner.py
@@ -287,7 +287,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message here:\n")
 shift = int(input("Type the shift number:\n"))
-
+shift = shift % 26
 # def encrypt(text, shift):
 #     cipher_text = ""
 #     for letter in text:
