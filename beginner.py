3936aea021ae3e73c5135c0b55cda68ba6304c74
jsparks404 <sparksj404@gmail.com>
2023-02-22 13:11:03 -0500

    nesting

--- beginner.py
+++ beginner.py
@@ -385,3 +385,31 @@ for student in scores:
         grades[student] = "Fail"
 
 # print(grades)
+
+
+
+
+        # Nesting
+
+travel_log = {
+    "France": {
+        "cities_visitied": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    "Germany": ["Berlin", "Hamburg"]
+}
+
+
+travel_log2 = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12,
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg"],
+        "total_visits": 5,
+    }
+]
+
