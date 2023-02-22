aea1688bcd3c08a977d344bc29cb66f115e00f09
jsparks404 <sparksj404@gmail.com>
2023-02-22 14:45:39 -0500

    calculator

--- beginner.py
+++ beginner.py
@@ -478,3 +478,41 @@ def format_name(f_name, l_name):
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
+
+
+
+            # Calculator
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add, 
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("What's the first number: "))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Pick an operation from the line above: ")
+num2 = int(input("What's the second number: "))
+
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
+
