8b5c39061452acde284e20553eff44e9a6768be9
jsparks404 <sparksj404@gmail.com>
2023-02-22 13:45:51 -0500

    silent bidding

--- beginner.py
+++ beginner.py
@@ -422,4 +422,29 @@ def add_new_country(country_visited, times_visited, cities_visited):
 
 add_new_country("Russia", 1, ["Moscow", "Sochi"])
 
-print(travel_log2)
+# print(travel_log2)
+
+
+
+        # Blind Auction
+bids = {}
+bidding_finished = False
+def highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+       bid_amount = bidding_record[bidder]
+       if bid_amount > highest_bid:
+           highest_bid = bid_amount
+           winner = bidder
+    print(f"The winner is {winner} with the bid of ${highest_bid}")
+
+while not bidding_finished:
+    name = input("What is your name: ")
+    price = int(input("What is your bid: $"))
+    bids[name] = price
+    should_continue = input("Any other bidders? 'Yes' or 'no': ")
+    if should_continue == "no":
+        bidding_finished = True
+        highest_bidder(bids)
+    
