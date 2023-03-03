88cef203d1ddfafe418e6685506559ea790075a7
jsparks404 <sparksj404@gmail.com>
2023-03-03 13:32:23 -0500

    object oriented programming

---
+++ intermediate/oop.py
@@ -0,0 +1,10 @@
+                # Object oriented programming
+            #breaking down code into smaller components to be more scalable
+
+        # Attributes: variable associated with a modeled object / things they have
+        # Methods: Functions associated with a particular modeled object / things they do
+
+        # Can have several objects modeled after the same type     Class(blueprint) --> object
+        # car = CarBlueprint() -> pascal case, 1st letter of word capitalized
+
+import turtle
